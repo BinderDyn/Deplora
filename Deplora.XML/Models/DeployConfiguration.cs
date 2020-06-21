a04311a233ed4da6aa0154336e9a171a7381461f
BinderDyn <marvler@gmx.de>
2020-06-21 21:52:12 +0200

    DeployConfiguration init

---
+++ Deplora.XML/Models/DeployConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deplora.XML.Models
+{
+    public class DeployConfiguration
+    {
+        /// <summary>
+        /// The path to deploy the files to
+        /// </summary>
+        public string DeployPath { get; set; }
+        /// <summary>
+        /// The custom name for this configuration
+        /// </summary>
+        public string Designation { get; set; }
+    }
+}
