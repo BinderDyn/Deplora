bd66bf4cc4722572f962092dc8a461ae373f0705
BinderDyn <marvler@gmx.de>
2020-06-23 07:46:53 +0200

    File manager and tree view init

---
+++ Deplora.DataAccess/FileManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Deplora.DataAccess
+{
+    public class FileManager
+    {
+        /// <summary>
+        /// Creates a backup for everything in the except the excluded directory/file names
+        /// </summary>
+        /// <param name="directoryInfo"></param>
+        /// <param name="exclude"></param>
+        public void Backup(DirectoryInfo directoryInfo, params string[] exclude)
+        {
+            if (directoryInfo == null) throw new InvalidOperationException("DirectoryInfo cannot be null!");
+            string temporaryDirectoryPath = CreateTemporaryDirectory(directoryInfo.FullName);
+            // TODO FileStructure
+        }
+
+        public string CreateTemporaryDirectory(string path)
+        {
+            string temporaryDirectoryName = string.Format("{0:yyyyMMdd}_temp", DateTime.Now);
+            string tempPath = Path.Combine(path, temporaryDirectoryName);
+            var dirInfo = Directory.CreateDirectory(tempPath);
+            return dirInfo.FullName;
+        }
+
+        public void ZipContents(DirectoryInfo directoryInfo, string outputPath)
+        {
+        }
+    }
+}
---
+++ Deplora.DataAccess/FileSystemNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Deplora.DataAccess
+{
+    public class FileSystemNode
+    {
+        public string Path { get; set; }
+        public IEnumerable<DirectoryInfo> Directories { get; set; }
+        public IEnumerable<FileInfo> FileInfos { get; set; }
+        public FileSystemNode Parent { get; set; }
+        public IEnumerable<FileSystemNode> Children { get; set; }
+    }
+}
