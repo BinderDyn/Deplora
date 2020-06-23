e9dbb0589c9f35175541511e2d9f437486912c2d
BinderDyn <marvler@gmx.de>
2020-06-23 22:37:37 +0200

    FileSystemNode + tests

---
+++ Deplora.DataAccess.TESTS/FileSystemNodeTests.cs
@@ -0,0 +1,128 @@
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Deplora.DataAccess.TESTS
+{
+    [TestClass]
+    public class FileSystemNodeTests
+    {
+        private string initialPath { get => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); }
+        private string testPath { get => Path.Combine(initialPath, "depth0"); }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // ARRANGE
+            var pathDepth1 = Path.Combine(testPath, "depth1");
+            var pathDepth1_2 = Path.Combine(testPath, "depth1_2");
+            var pathDepth2 = Path.Combine(pathDepth1, "depth2");
+            var fileStreams = new List<FileStream>();
+
+            Directory.CreateDirectory(testPath);
+            fileStreams.Add(File.Create(Path.Combine(testPath, "FileDepth0_1.txt")));
+            fileStreams.Add(File.Create(Path.Combine(testPath, "FileDepth0_2.txt")));
+            Directory.CreateDirectory(pathDepth1);
+            Directory.CreateDirectory(pathDepth1_2);
+            fileStreams.Add(File.Create(Path.Combine(pathDepth1, "FileDepth1_1.txt")));
+            fileStreams.Add(File.Create(Path.Combine(pathDepth1, "FileDepth1_2.txt")));
+            fileStreams.Add(File.Create(Path.Combine(pathDepth1_2, "FileDepth1_3.txt")));
+            Directory.CreateDirectory(pathDepth2);
+            fileStreams.Add(File.Create(Path.Combine(pathDepth2, "FileDepth2_1.txt")));
+            fileStreams.Add(File.Create(Path.Combine(pathDepth2, "FileDepth2_2.txt")));
+
+            foreach (var fs in fileStreams)
+            {
+                fs.Close();
+            }
+        }
+
+        [TestMethod]
+        public void GetNodesRecursively_Test()
+        {
+            // ACT
+            var node = FileSystemNode.GetNodesRecursively(new DirectoryInfo(testPath));
+
+            // ASSERT
+            Assert.IsNotNull(node);
+            Assert.AreEqual(0, node.Depth);
+        }
+
+        [TestMethod]
+        public void GetNodesRecursively_TestDepth()
+        {
+            // ACT
+            var node = FileSystemNode.GetNodesRecursively(new DirectoryInfo(testPath));
+
+            // ASSERT
+            var nodeChildrenDepth1 = node.Children.ToList();
+            var nodeChildDepth2 = nodeChildrenDepth1.First().Children.First();
+            Assert.IsNotNull(nodeChildDepth2);
+            Assert.AreEqual(2, nodeChildDepth2.Depth);
+        }
+
+        [TestMethod]
+        public void GetNodesAtDepthZero_Test()
+        {
+            // ARRANGE
+            var node = FileSystemNode.GetNodesRecursively(new DirectoryInfo(testPath));
+
+            // ACT
+            var nodes = node.GetNodesAtDepth(0);
+
+            // ASSERT
+            Assert.AreEqual(1, nodes.Count());
+        }
+
+        [TestMethod]
+        public void GetNodesAtDepthOne_Test()
+        {
+            // ARRANGE
+            var node = FileSystemNode.GetNodesRecursively(new DirectoryInfo(testPath));
+
+            // ACT
+            var nodes = node.GetNodesAtDepth(1);
+
+            // ASSERT
+            Assert.AreEqual(2, nodes.Count());
+        }
+
+        [TestMethod]
+        public void GetNodesAtDepthNegative_Test()
+        {
+            // ARRANGE
+            var node = FileSystemNode.GetNodesRecursively(new DirectoryInfo(testPath));
+
+            // ACT
+            var nodes = node.GetNodesAtDepth(-1);
+
+            // ASSERT
+            Assert.AreEqual(0, nodes.Count());
+        }
+
+        [TestMethod]
+        public void GetNodesAtDepthTooDeep_Test()
+        {
+            // ARRANGE
+            var node = FileSystemNode.GetNodesRecursively(new DirectoryInfo(testPath));
+
+            // ACT
+            var nodes = node.GetNodesAtDepth(5);
+
+            // ASSERT
+            Assert.AreEqual(0, nodes.Count());
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            if (Directory.Exists(testPath)) 
+            {
+                Directory.Delete(testPath, true);
+            }
+        }
+    }
+}
--- Deplora.DataAccess/FileSystemNode.cs
+++ Deplora.DataAccess/FileSystemNode.cs
@@ -1,16 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Deplora.DataAccess
 {
     public class FileSystemNode
     {
+        public FileSystemNode()
+        {
+            this.Directories = new List<DirectoryInfo>();
+            this.FileInfos = new List<FileInfo>();
+            this.Children = new List<FileSystemNode>();
+        }
+
+        /// <summary>
+        /// Returns all containing directory information and their files recursively
+        /// </summary>
+        /// <param name="directoryInfo">the initial directory to start</param>
+        /// <param name="depth">The depth of the directory where 0 equals root or starting point</param>
+        /// <param name="parent">The parent node if available</param>
+        /// <returns></returns>
+        public static FileSystemNode GetNodesRecursively(DirectoryInfo directoryInfo, int depth = 0, FileSystemNode parent = null)
+        {
+            var node = new FileSystemNode();
+            var containingDirectories = directoryInfo.GetDirectories();
+            node.Directories = containingDirectories.ToList();
+            node.FileInfos = directoryInfo.GetFiles().ToList();
+            node.Path = directoryInfo.FullName;
+            node.Depth = depth;
+            if (parent != null) node.Parent = parent;
+            foreach (var directory in containingDirectories)
+            {
+                node.Children.Add(GetNodesRecursively(directory, depth+1, node));
+            }
+            return node;
+        }
+
+        /// <summary>
+        /// Returns nodes at this or at depth above or below
+        /// </summary>
+        /// <param name="depth"></param>
+        /// <returns></returns>
+        public IEnumerable<FileSystemNode> GetNodesAtDepth(int depth)
+        {
+            if (depth == this.Depth)
+            {
+                return new List<FileSystemNode>() { this };
+            }
+            else if (depth < this.Depth && this.Parent != null)
+            {
+                return Parent.GetNodesAtDepth(depth);
+            }
+            else if (depth < this.Depth && this.Parent == null)
+            {
+                return new List<FileSystemNode>();
+            }
+            else
+            {
+                var nodesBelow = this.Children.SelectMany(c => c.GetNodesAtDepth(depth));
+                if (nodesBelow.Any()) return nodesBelow;
+                else return new List<FileSystemNode>();
+            }
+        }
+
+        public int Depth { get; set; }
         public string Path { get; set; }
-        public IEnumerable<DirectoryInfo> Directories { get; set; }
-        public IEnumerable<FileInfo> FileInfos { get; set; }
+        public List<DirectoryInfo> Directories { get; set; }
+        public List<FileInfo> FileInfos { get; set; }
         public FileSystemNode Parent { get; set; }
-        public IEnumerable<FileSystemNode> Children { get; set; }
+        public List<FileSystemNode> Children { get; set; }
     }
 }
