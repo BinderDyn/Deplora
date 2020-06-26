713f2337424a5dc9c0aef594db58b8399203f444
BinderDyn <marvler@gmx.de>
2020-06-26 22:20:06 +0200

    Starting/Stopping website + tests

--- Deplora.IIS/IISManager.cs
+++ Deplora.IIS/IISManager.cs
@@ -11,24 +11,59 @@ namespace Deplora.IIS
         private readonly string iisPath;
         private readonly string webSiteName;
 
-        public string startApplicationPoolCommand { get => $"{iisPath}appcmd start apppool /apppool.name:\"{appPoolName}\""; }
-        public string stopApplicationPoolCommand { get => $"{iisPath}appcmd stop apppool /apppool.name:\"{appPoolName}\""; }
-        public string startWebSiteCommand { get => $"{iisPath}appcmd start site /site.name:\"{appPoolName}\""; }
-        public string stopWebSiteCommand { get => $"{iisPath}appcmd stop site /site.name:\"{appPoolName}\""; }
+        public IISManager(string appPoolName, string iisPath, string webSiteName)
+        {
+            this.appPoolName = appPoolName;
+            this.iisPath = iisPath;
+            this.webSiteName = webSiteName;
+        }
+
+        private string startApplicationPoolCommand { get => $"{iisPath}appcmd start apppool /apppool.name:\"{appPoolName}\""; }
+        private string stopApplicationPoolCommand { get => $"{iisPath}appcmd stop apppool /apppool.name:\"{appPoolName}\""; }
+        private string startWebSiteCommand { get => $"{iisPath}appcmd start site /site.name:\"{webSiteName}\""; }
+        private string stopWebSiteCommand { get => $"{iisPath}appcmd stop site /site.name:\"{webSiteName}\""; }
 
         /// <summary>
         /// Executes the commands given and terminates
         /// </summary>
         /// <param name="command"></param>
-        public void ExecuteCommands(string command)
+        public bool ExecuteCommand(string command)
         {
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
             startInfo.FileName = "cmd.exe";
             startInfo.Arguments = $"/C {command}";
+            startInfo.Verb = "runas";
+            startInfo.UseShellExecute = true;
             process.StartInfo = startInfo;
             process.Start();
+            process.WaitForExit();
+            if (process.ExitCode == 0)
+            {
+                return true;
+            }
+            else throw new Exception(string.Format("Process terminated with exit code {0}", process.ExitCode));
+        }
+
+        public void StartWebsite()
+        {
+            ExecuteCommand(startWebSiteCommand);
+        }
+
+        public void StopWebsite()
+        {
+            ExecuteCommand(stopWebSiteCommand);
+        }
+
+        public void StartAppPool()
+        {
+            ExecuteCommand(startApplicationPoolCommand);
+        }
+
+        public void StopAppPool()
+        {
+            ExecuteCommand(stopApplicationPoolCommand);
         }
     }
 }
