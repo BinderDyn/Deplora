64b54872a62a660a621ff89a3bd46a50f65b0f54
BinderDyn <marvler@gmx.de>
2020-07-04 12:48:08 +0200

    Test stubs

---
+++ Deplora.App.TESTS/ConfigurationControllerTests.cs
@@ -0,0 +1,50 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Deplora.App.TESTS
+{
+    [TestClass]
+    public class ConfigurationControllerTests
+    {
+        [TestMethod]
+        public void GetCurrentSettings_Test()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetDeployConfigurations_Test()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetDeployConfiguration_Test()
+        {
+
+        }
+
+        [TestMethod]
+        public void CreateDeployConfiguration_Test()
+        {
+
+        }
+
+        [TestMethod]
+        public void UpdateDeployConfiguration_Test()
+        {
+
+        }
+
+        [TestMethod]
+        public void CreateDefaultConfigurationWithCustomPath_Test()
+        {
+
+        }
+
+        [TestMethod]
+        public void SaveApplicationConfiguration_Test()
+        {
+
+        }
+    }
+}
