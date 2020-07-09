40e2968cbd57d064ef2eff3604aa3a0553ccd9d3
BinderDyn <marvler@gmx.de>
2020-07-09 07:44:47 +0200

    icon + version number in main menu

--- Deplora.WPF/ViewModels/MainWindowViewModel.cs
+++ Deplora.WPF/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using Deplora.WPF.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows.Input;
 
@@ -18,9 +19,21 @@ namespace Deplora.WPF.ViewModels
         {
             // Create Settings if not available at first start
             _ = ConfigurationController.GetCurrentSettings();
+
+            this.Version = GetSemanticVersionString();
             this.ShowAppSettings = new RelayCommand(ShowAppSettingsWindow);
             this.ShowDeployConfigurations = new RelayCommand(ShowDeployConfigurationsWindow);
             this.LaunchDeploy = new RelayCommand(ShowLaunchManualDeploy, CanLaunchDeploy);
+        }
+
+        private string version;
+        public string Version { get => version; set => SetProperty(ref version, value); }
+        public string WindowTitleWithVersion { get => string.Format("Deplora v{0}", this.Version); }
+
+        private string GetSemanticVersionString()
+        {
+            var applicationVersion = Assembly.GetExecutingAssembly().GetName().Version;
+            return string.Format("{0}.{1}.{2}", applicationVersion.Major, applicationVersion.Minor, applicationVersion.Build);
         }
 
         private void ShowAppSettingsWindow()
