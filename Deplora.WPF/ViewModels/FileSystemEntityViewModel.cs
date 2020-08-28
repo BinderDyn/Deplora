a7dd485711402addc2ab5afd77192085a6ccb715
BinderDyn <marvler@gmx.de>
2020-08-28 07:47:22 +0200

    FolderBrowser + FileSystemEntityViewModel

---
+++ Deplora.WPF/ViewModels/FileSystemEntityViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deplora.WPF.ViewModels
+{
+    public class FileSystemEntityViewModel : ViewModelBase
+    {
+
+        private FileSystemEntityType type;
+        public FileSystemEntityType Type { get => type; set => SetProperty(ref type, value); }
+    }
+
+    public enum FileSystemEntityType 
+    {
+        Folder = 0,
+        Drive = 1,
+        File = 2
+    }
+
+}
---
+++ Deplora.WPF/ViewModels/FolderBrowserDialogViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+
+namespace Deplora.WPF.ViewModels
+{
+    public class FolderBrowserDialogViewModel : ViewModelBase
+    {
+        public FolderBrowserDialogViewModel()
+        {
+            this.directories = new ObservableCollection<DirectoryInfo>();
+            directories.CollectionChanged += Directories_CollectionChanged;
+        }
+        
+
+        private DirectoryInfo selectedDirectory;
+        public DirectoryInfo SelectedDirectory { get => selectedDirectory; set => SetProperty(ref selectedDirectory, value); }
+
+        private ObservableCollection<DirectoryInfo> directories;
+        public ObservableCollection<DirectoryInfo> Directories { get => directories; }
+        private void Directories_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            SetCollection("Directories");
+        }
+    }
+}
