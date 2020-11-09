c84d5b01d18981bfc3525ca0cc519b48328c006b
BinderDyn <marvler@gmx.de>
2020-11-09 21:05:04 +0100

    gitignore

--- Deplora.WPF/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ Deplora.WPF/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -70,7 +70,7 @@ namespace Deplora.WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Deplora.WPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Deplora.WPF;V1.0.1.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
